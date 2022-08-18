dc52e7684c82fd19a97995cb57f369c00defb9fa
tida1732 <79642133+adityaagg7@users.noreply.github.com>
2022-08-18 23:23:51 +0530

    feat: snap road points

--- SpreadSheet/GetPointsAPI/views.py
+++ SpreadSheet/GetPointsAPI/views.py
@@ -18,14 +18,34 @@ def getPoints(request):
  
 @api_view(['POST'])
 def snapPoly(request):
-    points=request.data["point"]
+    
+    points=request.POST["point"]
+  
+    # print(points)
+    point=points.split(",")
+    # print(point)
+    for j in range(len(point)):
+        i=point[j]
+        i=i.replace("(","")
+        i=i.replace(")","")
+        print(i,"here","\n")
+        point[j]=float(i)
+        print("FLOATAFEAGSRTGWERE")
+
+    combPoint=[]
+    a=[]
+    for i in range(1,len(point),2):
+        a.append(point[i-1])
+        a.append(point[i])
+        combPoint.append(a)
+    print(combPoint)
     points_snapped=[]
     prev=0
-    for i in range(100,len(points),100):
-        points_snapped.append(gmc.snap_to_roads(path=points[prev:i],interpolate=True))
+    for i in range(100,len(combPoint),100):
+        points_snapped.append(gmc.snap_to_roads(path=combPoint[prev:i],interpolate=True))
         prev=i
-    points_snapped.append(gmc.snap_to_roads(path=points[prev:len(points)],interpolate=True))
-    print(points_snapped)
+    points_snapped.append(gmc.snap_to_roads(path=combPoint[prev:len(combPoint)],interpolate=True))
+    print(points_snapped,"AFSGHGR")
     return Response(points_snapped)
     
         
