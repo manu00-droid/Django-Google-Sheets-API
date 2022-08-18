a31afd510aa344557816bb85cbdb7765729f9cb4
tida1732 <79642133+adityaagg7@users.noreply.github.com>
2022-08-18 11:23:51 +0530

    feat:snappedToRoads

--- SpreadSheet/GetPointsAPI/views.py
+++ SpreadSheet/GetPointsAPI/views.py
@@ -3,9 +3,9 @@ from rest_framework.response import Response
 import gspread
 import requests
 
-global points_array
-
-
+import googlemaps as gm
+key="AIzaSyDhwmI3NkM9HWlohjrZQksbAetQ-M7ZOvQ"
+gmc=gm.Client(key=key)
 @api_view(['GET'])
 def getPoints(request):
     gc = gspread.service_account(filename='GetPointsAPI/testproject-347704-46bae9ce3d03.json')
@@ -13,16 +13,24 @@ def getPoints(request):
     sh = gc.open("test_sheet")
     print(sh)
     points_array=sh.sheet1.get_all_values()
+    points_snapped=gmc.snap_to_roads(path=points_array,interpolate=True)
+    print(points_snapped)
     return Response(sh.sheet1.get_all_values())
     
+# def pointsToString(point_array):
+#     points_string=""
+#     for i in point_array:
+#         current_points_string="%2C".join(str(i[0]),str(i[1]))
+#         points_string="%7C".join(points_string,current_points_string)
+#     return points_string
 
-def snapToRoads():
-    points=""
-    url = f"https://roads.googleapis.com/v1/snapToRoads?path={points}&interpolate=true&key=AIzaSyDhwmI3NkM9HWlohjrZQksbAetQ-M7ZOvQ"
+# def snapToRoads():
+#     points=""
+#     url = f"https://roads.googleapis.com/v1/snapToRoads?path={points}&interpolate=true&key=AIzaSyDhwmI3NkM9HWlohjrZQksbAetQ-M7ZOvQ"
 
-    payload = {}
-    headers = {}
+#     payload = {}
+#     headers = {}
 
-    response = requests.request("GET", url, headers=headers, data=payload)
+#     response = requests.request("GET", url, headers=headers, data=payload)
 
-    print(response.text)
+#     print(response.text)
