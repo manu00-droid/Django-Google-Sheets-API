32b4e1b94e87fc3613b1be942510a99c287a5641
tida1732 <79642133+adityaagg7@users.noreply.github.com>
2022-08-20 12:46:25 +0530

    Update views.py

--- SpreadSheet/GetPointsAPI/views.py
+++ SpreadSheet/GetPointsAPI/views.py
@@ -1,31 +1,37 @@
+from urllib import response
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import gspread
 import requests
 import googlemaps as gm
+import json
 
 key="AIzaSyDhwmI3NkM9HWlohjrZQksbAetQ-M7ZOvQ"
 gmc=gm.Client(key=key)
 @api_view(['GET'])
 def getPoints(request):
     gc = gspread.service_account(filename='GetPointsAPI/testproject-347704-46bae9ce3d03.json')
-    print(gc)
+    # print(gc)
     sh = gc.open("test_sheet")
-    print(sh)
+    # print(sh) 
     points_array=sh.sheet1.get_all_values()
-
+    print("sheet sent!")
     return Response(points_array)
  
 @api_view(['POST'])
 def snapPoly(request):
     
     points=request.POST["point"]
-  
+
+
+    
+    
     points=points.replace("(","")
     point=points.split("),")
     combPoint=point
     combPoint[-1]=combPoint[-1].replace(")","")
-    print(combPoint[-1])
+    
+    print(combPoint)
     points_snapped=[]
     prev=0
     for i in range(100,len(combPoint),100):
@@ -33,7 +39,7 @@ def snapPoly(request):
         prev=i
 
     points_snapped+=gmc.snap_to_roads(path=combPoint[prev:len(combPoint)],interpolate=True)
-    print(points_snapped[0])
+    # print(points_snapped[0])
     return Response(points_snapped)
     
         
