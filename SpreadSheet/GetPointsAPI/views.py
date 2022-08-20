293ec53421818a446fdd8e224f6407dfcc832d4b
tida1732 <79642133+adityaagg7@users.noreply.github.com>
2022-08-20 14:10:01 +0530

    feat: addPoints

--- SpreadSheet/GetPointsAPI/views.py
+++ SpreadSheet/GetPointsAPI/views.py
@@ -3,11 +3,19 @@ from rest_framework.response import Response
 import gspread
 import requests
 import googlemaps as gm
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+import json
+=======
+from SpreadSheet import settings
 
-key="AIzaSyDhwmI3NkM9HWlohjrZQksbAetQ-M7ZOvQ"
-gmc=gm.Client(key=key)
+>>>>>>> eddee071277bb8134c336641bdbacd6dba9e7f0f
+>>>>>>> parent of 6ffb0a3 (Revert "sep")
+
 @api_view(['GET'])
 def getPoints(request):
+<<<<<<< HEAD
     gc = gspread.service_account(filename='GetPointsAPI/testproject-347704-46bae9ce3d03.json')
     print(gc)
     sh = gc.open("test_sheet")
@@ -37,3 +45,19 @@ def snapPoly(request):
     return Response(points_snapped)
     
         
+=======
+    sheet = settings.google_sheet
+    points_array = sheet.sheet1.get_all_values()
+    return Response(points_array)
+
+
+@api_view(['POST'])
+def addPoint(request):
+    sheet = settings.google_sheet
+    lat = request.data['lat']
+    lng = request.data['lng']
+    length = len(sheet.sheet1.get_all_values())
+    sheet.sheet1.update_cell(length + 1, 1, lat)
+    sheet.sheet1.update_cell(length + 1, 2, lng)
+    return Response("done")
+>>>>>>> eddee071277bb8134c336641bdbacd6dba9e7f0f
